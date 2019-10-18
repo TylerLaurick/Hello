07d7725844dd32bed46365130009644cf6982e82
TylerLaurick <56735320+TylerLaurick@users.noreply.github.com>
2019-10-18 11:43:29 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+// First program in Java
+/*
+    Hello.java
+*/
+
+public class Hello {
+    public static void main(String[] args) {
+        
+        int x= 0;
+        while(x < 10) {
+            System.out.println("Hello World");
+            x++;
+ 
+        }
+    }
+}
